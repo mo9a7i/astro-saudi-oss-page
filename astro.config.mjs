4b29e001a5a3ff2edf00b534815ae92812edc2c3
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-31 23:22:52 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mo9a7i.github.io',
+  base: '/astro-saudi-oss-page',
   integrations: [tailwind()]
 });
