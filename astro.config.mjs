0f573c96fe57d30dedc5c3d8b0674ad7b30ea953
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-31 23:39:26 +0300

    fix: images ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mo9a7i.github.io',
   base: '/astro-saudi-oss-page',
-  integrations: [tailwind()]
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+  })]
 });
